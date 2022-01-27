12421c1a697004c4e705425957808cf2e225cc5e
tech-zero <tech-zero@outlook.com>
2022-01-26 21:10:48 -0700

    added data

---
+++ Phase-5/my_maths.py
@@ -0,0 +1,10 @@
+def adder(a, b):
+    return a + b
+
+
+def subtractor(a, b):
+    return a - b
+
+
+def the_multiplier(a, b):
+    return a * b
---
+++ Phase-5/pull_data.py
@@ -0,0 +1,6 @@
+import csv
+from scrapli.driver.core import IOSXEDriver
+from inv import DEVICES
+
+for device in DEVICES:
+    print(device)
