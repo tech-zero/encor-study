a1fae674a2e5c96bbeac14fbf6e348ced8a51e48
tech-zero <tech-zero@outlook.com>
2022-01-31 23:37:15 -0700

    repo update

---
+++ function1.py
@@ -0,0 +1,7 @@
+def warning_text():
+    print("This network belongs to CBT NUGGETS - don't mess around with it")
+
+warning_text()
+print("This is an example of a warning...")
+print("you have been warned!")
+warning_text()
