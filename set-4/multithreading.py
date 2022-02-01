f409b403714136c22d4076218f247ba29a6f5189
tech-zero <98118886+tech-zero@users.noreply.github.com>
2022-02-01 00:17:48 -0700

    created multithreading.py to repo

---
+++ set-4/multithreading.py
@@ -0,0 +1,33 @@
+from concurrent.futures import ThreadPoolExecutor
+from time import perf_counter
+from scrapli.driver.core import IOSXEDriver
+from inv import DEVICES
+
+start = perf_counter()
+
+def send_cmd(device):
+    with IOSXEDriver(
+        host=device["host"],
+        auth_username="admin",
+        auth_password="cisco",
+        auth_strict_key=False,
+        ssh_config_file=True,
+    ) as conn:
+        response = conn.send_command("show version")
+        return response.result
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(send_cmd, DEVICES)
+
+    for result in results:
+        print(result)
+
+    end = perf_counter()
+    total_time = end - start
+    print(total_time)
+
+'''
+Note:
+start() is the method within the thread class that is used to begin the execution of thread.
+'''
