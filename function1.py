090299bede3c8916bd4305b19a065c75f2dd822d
tech-zero <98118886+tech-zero@users.noreply.github.com>
2022-02-01 00:14:09 -0700

    Delete function1.py

--- function1.py
+++
@@ -1,7 +0,0 @@
-def warning_text():
-    print("This network belongs to CBT NUGGETS - don't mess around with it")
-
-warning_text()
-print("This is an example of a warning...")
-print("you have been warned!")
-warning_text()
